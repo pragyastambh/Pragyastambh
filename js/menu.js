80c9a48be2b66fa4b80bf03741d206c02349a57c
PragyaStambh <inishad346@gmail.com>
2026-07-08 16:40:31 +0530

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,1 +1,9 @@
 
+const menuToggle = document.getElementById("menu-toggle");
+const navbar = document.getElementById("navbar");
+
+if(menuToggle){
+    menuToggle.addEventListener("click", () => {
+        navbar.classList.toggle("show");
+    });
+}
